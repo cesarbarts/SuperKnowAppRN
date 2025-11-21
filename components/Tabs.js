25a1a60ea88f3b0d4456f1af122eb02748bd3bce
Cesar Buril <cesarburil@MacBook-Pro-de-Cesar.local>
2025-11-21 17:43:15 -0300

    Correção ícones tabbar

--- components/Tabs.js
+++ components/Tabs.js
@@ -2,12 +2,39 @@ import React from 'react';
 import FilmePage from './Filme';
 import AboutPage from './About';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@react-native-vector-icons/feather';
 const Tab = createBottomTabNavigator();
 function TabsNavigator() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Filmes" component={FilmePage}></Tab.Screen>
-      <Tab.Screen name="Sobre" component={AboutPage}></Tab.Screen>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: '#1423f0ff',
+          borderTopColor: '#00000000',
+        },
+        tabBarActiveTintColor: '#fbff00ff',
+        tabBarInactiveTintColor: '#fff',
+      }}
+    >
+      <Tab.Screen
+        name="Filmes"
+        component={FilmePage}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <Feather name="film" size={size} color={color}></Feather>
+          ),
+        }}
+      ></Tab.Screen>
+      <Tab.Screen
+        name="Sobre"
+        component={AboutPage}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <Feather name="user" size={size} color={color}></Feather>
+          ),
+        }}
+      ></Tab.Screen>
     </Tab.Navigator>
   );
 }
